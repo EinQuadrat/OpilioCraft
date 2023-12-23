80ee27ee20238562e50c428af365f88c4b65bab8
Carsten Schäfer <carsten.schaefer@mailbox.org>
2023-12-23 15:51:19 +0100

    Projektdateien hinzufügen.

---
+++ Settings.fs
@@ -0,0 +1,8 @@
+﻿namespace OpilioCraft
+
+open System
+
+[<RequireQualifiedAccess>]
+module Settings =
+    // location of app specific data
+    let AppDataLocation = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "OpilioCraft")
