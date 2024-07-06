75085d59725a0345f5354fa02308d6370f9fe416
Carsten Schäfer <carsten.schaefer@mailbox.org>
2024-07-06 09:33:11 +0200

    Added SolutionIdentifier as separate literal.

--- Settings.fs
+++ Settings.fs
@@ -4,5 +4,8 @@ open System
 
 [<RequireQualifiedAccess>]
 module Settings =
+    [<Literal>]
+    let SolutionIdentifier = "OpilioCraft"
+
     // location of app specific data
-    let AppDataLocation = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "OpilioCraft")
+    let AppDataLocation = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), SolutionIdentifier)
